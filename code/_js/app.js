597271e9e3c4260dbdc682b2f1160e1f6ea72e2a
Victor Martins <martinsvictor6781@gmail.com>
2022-06-21 09:33:40 -0300

    ajustes na geração do conteúdo

--- code/_js/app.js
+++ code/_js/app.js
@@ -1,16 +1,16 @@
-let headerContainer = document.getElementById('header')
-let mainContainer = document.getElementById('main')
-let footerContainer = document.getElementById('footer')
-
-let header = new Header()
+let body = document.getElementsByTagName('body')[0]
+let mainContainer = document.createElement('main')
 
 let sectionContent = new SectionContent("Enfrente as profundezas de um reino esquecido", "Como o enigmático Cavaleiro, você atravessará as profundezas, desvendará seus mistérios e vencerá seus males", "_images/The_Knight.png", "Baixe o jogo", "steam");
 
+let header = new Header()
 let section = new Section(sectionContent)
-
 let footer =  new Footer("Copyright © 2022, Todos os direitos reservados")
 
+body.appendChild(header.getComponents())
 
-headerContainer.appendChild(header.getComponents())
 mainContainer.appendChild(section.getComponents())
-footerContainer.appendChild(footer.getComponents())
+body.appendChild(mainContainer)
+mainContainer.classList.add('container')
+
+body.appendChild(footer.getComponents())
